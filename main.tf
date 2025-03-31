7322c58b11c0e879b36178925520e8307b0f1f63
llanard <louis.lanard@louis.lanard-LLQLFK7RWT>
2025-03-31 11:14:49 +0200

    changing files

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region  = "eu-west-3"
+  
+}
+
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,8 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.87.0"
-      region  = "eu-west-3"
     }
   }
 
